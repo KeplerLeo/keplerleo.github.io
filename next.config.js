b4efb031ebfff7e96eff2cbf11f5ac02cb66e6d8
Leonardo Kepler <leokepler@gmail.com>
2023-01-05 10:56:50 -0300

    chore: setting up action workflow to build

--- next.config.js
+++ next.config.js
@@ -1,3 +1,21 @@
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/src'
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 module.exports = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  images: {
+    loader: 'imgix',
+    path: 'the "domain" of your Imigix source',
+  },
 }
